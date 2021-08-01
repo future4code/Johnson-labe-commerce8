9424d37fbbbb92cfcaa9c2c22627d3cf6f1009f1
andersonheidrich <andersonheidrich@hotmail.com>
2021-08-01 17:04:06 -0300

    adicionando estilizacoes novas

--- src/components/cardViagem.js
+++ src/components/cardViagem.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-// import marte from './img/marte2.jpg'
 
 const Card = styled.div`
 display: flex;
@@ -12,33 +11,31 @@ padding-bottom: 20px;
 const CardShadow = styled.div`
   box-shadow: 2px 2px 15px white;
   border-radius: 10px;
-  height: 250px;
-  width: 250px;
+  height: 280px;
+  width: 280px;
 `;
 
 const BorderImg = styled.img`
   border-radius: 10px;
-  max-height: auto;
-  max-width: auto;
+  width: 280px;
+  height: 280px;
 `;
 
-const CardViagem = () => {
+const CardViagem = (props) => {
   return (
-    <div className="">
-      <div>
+    <div>
       <Card>
         <CardShadow>
-          <BorderImg src="https://picsum.photos/250/250?a=1" alt="" />
+          <BorderImg src={props.viagem} alt="" />
         </CardShadow>
-
+        
         <div>
-          <p>Viagem</p>
-          <p>R$ 100,00</p>
+          <p>{props.texto}</p>
+          <p>{props.preco}</p>
           <button>Adicionar ao carrinho</button>
         </div>
       </Card>
-      </div>
-    </div>
+    </div>  
   );
 };
 
